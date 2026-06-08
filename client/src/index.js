bc3abb3cb894ae028675a210e9ce6d8714d63e28
Saksham Negi <sakshamnegi05@email.com>
2026-06-08 10:39:22 +0530

    feat: React app setup with global styles

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+// client/src/index.js
+// The entry point React uses to render our app into the HTML page.
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
